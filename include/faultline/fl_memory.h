a4fbae731844565ae1243385eb16825aff210d73
Douglas Cuthbertson <doug.cuthbertson@gmail.com>
2026-08-15 07:21:54 -0400

    Add FL_ALIGNED_ALLOC to the memory selector
    FL_ALIGNED_ALLOC joins FL_MALLOC, FL_FREE, FL_CALLOC and FL_REALLOC in
    fl_memory.h. Under FL_PLATFORM_BUILD it expands to flp_aligned_alloc
    with __FILE__ and __LINE__ appended; otherwise it expands to plain
    aligned_alloc, which fla_memory_service.h shadows into a call through
    g_fla_memory_service.

--- include/faultline/fl_memory.h
+++ include/faultline/fl_memory.h
@@ -8,18 +8,20 @@
 
 #if defined(FL_PLATFORM_BUILD)
 #include <flp_memory_service.h> // IWYU pragma: export
-#define FL_MALLOC(SZ)       flp_malloc((SZ), __FILE__, __LINE__)
-#define FL_FREE(PTR)        flp_free((PTR), __FILE__, __LINE__)
-#define FL_CALLOC(N, SZ)    flp_calloc((N), (SZ), __FILE__, __LINE__)
-#define FL_REALLOC(PTR, SZ) flp_realloc((PTR), (SZ), __FILE__, __LINE__)
+#define FL_MALLOC(SZ)            flp_malloc((SZ), __FILE__, __LINE__)
+#define FL_FREE(PTR)             flp_free((PTR), __FILE__, __LINE__)
+#define FL_CALLOC(N, SZ)         flp_calloc((N), (SZ), __FILE__, __LINE__)
+#define FL_REALLOC(PTR, SZ)      flp_realloc((PTR), (SZ), __FILE__, __LINE__)
+#define FL_ALIGNED_ALLOC(AL, SZ) flp_aligned_alloc((AL), (SZ), __FILE__, __LINE__)
 #else                                     // Application/DLL build
 #include <faultline/fla_memory_service.h> // IWYU pragma: export
-// fla_memory_service.h already redefines malloc/free/calloc/realloc as macros
-// that route through g_fla_memory_service.
-#define FL_MALLOC(SZ)       malloc(SZ)
-#define FL_FREE(PTR)        free(PTR)
-#define FL_CALLOC(N, SZ)    calloc((N), (SZ))
-#define FL_REALLOC(PTR, SZ) realloc((PTR), (SZ))
+// fla_memory_service.h already redefines malloc/free/calloc/realloc/aligned_alloc
+// as macros that route through g_fla_memory_service.
+#define FL_MALLOC(SZ)            malloc(SZ)
+#define FL_FREE(PTR)             free(PTR)
+#define FL_CALLOC(N, SZ)         calloc((N), (SZ))
+#define FL_REALLOC(PTR, SZ)      realloc((PTR), (SZ))
+#define FL_ALIGNED_ALLOC(AL, SZ) aligned_alloc((AL), (SZ))
 #endif // FL_PLATFORM_BUILD
 
 #endif // FL_MEMORY_H_
